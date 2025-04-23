48c5852f304fdd2a5dce7ab18a48491eeda70424
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-04-23 12:00:42 +0200

    refactor: restructure main js script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,44 +2,44 @@ import $ from 'jquery';
 import 'bootstrap';
 import './flexible.pagination.js'
 
-const animatedElements = [
-    ...document.querySelectorAll('svg.icon'),
-    ...document.querySelectorAll('ul.video-group li')
-];
+if (window.location.pathname === '/') {
+    const animatedElements = [
+        ...document.querySelectorAll('svg.icon'),
+        ...document.querySelectorAll('ul.video-group li')
+    ];
 
-animatedElements.forEach((el) => {
-    el.addEventListener('mouseenter', () => {
-        const randomDelay = Math.random() * 30; // random delay up to 30s
-        el.style.animationDelay = `-${randomDelay}s`;
-    }, { once: true });
-});
+    animatedElements.forEach((el) => {
+        el.addEventListener('mouseenter', () => {
+            const randomDelay = Math.random() * 30;
+            el.style.animationDelay = `-${randomDelay}s`;
+        }, { once: true });
+    });
+} else {
+    // From: https://github.com/lay295/TwitchDownloader/blob/d63f861d4cac4d3408fc7a31569cc10e63678ad5/TwitchDownloaderCore/Resources/chat-template.html#L99 (MIT)
 
-// From: https://github.com/lay295/TwitchDownloader/blob/d63f861d4cac4d3408fc7a31569cc10e63678ad5/TwitchDownloaderCore/Resources/chat-template.html#L99 (MIT)
+    /* https://stackoverflow.com/a/63270816 */
+    const convertColor = (v) => {
+        const val = v / 255;
+        return val <= 0.03928
+            ? val / 12.92
+            : Math.pow((val + 0.055) / 1.055, 2.4);
+    }
 
-/* https://stackoverflow.com/a/63270816 */
-const convertColor = (v) => {
-    const val = v / 255;
-    return val <= 0.03928
-        ? val / 12.92
-        : Math.pow((val + 0.055) / 1.055, 2.4);
-}
+    const getLuminance = (values) => {
+        const r = convertColor(values[0]);
+        const g = convertColor(values[1]);
+        const b = convertColor(values[2]);
 
-const getLuminance = (values) => {
-    const r = convertColor(values[0]);
-    const g = convertColor(values[1]);
-    const b = convertColor(values[2]);
+        return Number(0.2126 * r + 0.7152 * g + 0.0722 * b);
+    };
 
-    return Number(0.2126 * r + 0.7152 * g + 0.0722 * b);
-};
+    const getContrastRatio = (lumA, lumB) => {
+        return (Math.max(lumA, lumB) + 0.05) / (Math.min(lumA, lumB) + 0.05);
+    };
 
-const getContrastRatio = (lumA, lumB) => {
-    return (Math.max(lumA, lumB) + 0.05) / (Math.min(lumA, lumB) + 0.05);
-};
+    const back_color = [255, 255, 255];
+    const back_luminance = getLuminance(back_color);
 
-const back_color = [255, 255, 255];
-const back_luminance = getLuminance(back_color);
-
-if (window.location.pathname !== '/') {
     $('.comment-author').each(function (i, obj) {
         let colorRgb = $(obj).css('color');
         let color = colorRgb.substring(4, colorRgb.length - 1).replace(/ /g, '').split(',');
